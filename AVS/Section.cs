4eb17d8e47bbd79d78d0031d14d6a2c6f405d1e1
kruopalt <dogas20@gmail.com>
2017-03-04 14:10:16 +0200

    New class Section + main code

---
+++ AVS/Section.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVS
+{
+    class Section
+    {
+        public Shelf[] shelfs { get; private set; }
+        public int tierCount { get; private set; }
+        public int sectionNumber { get; private set; }
+    }
+}
