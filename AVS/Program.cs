00f180ac79534cc1584643330d477b43bc670d09
kruopalt <dogas20@gmail.com>
2017-03-04 11:59:56 +0200

    Kalibracijos pradžia

--- AVS/Program.cs
+++ AVS/Program.cs
@@ -8,8 +8,19 @@ namespace AVS
 {
 	class Program
 	{
-		static void Main(string[] args)
+        const string DataFilePath = "duomenys.txt";
+
+        static void Main(string[] args)
 		{
-		}
+            GreenHouse greenHouse = new GreenHouse();
+            Simulation simulation = new Simulation();
+            double AccesKode = Calibration(simulation);
+        }
+        static double Calibration(Simulation sim)
+        {
+            GreenHouse.FillWithData(DataFilePath); // Atminties struktūra užkrauna duombazės informacija
+            GreenHouse.CheckSensors(sim); // Tikrinima šiltnamio sensoriai, pagal paduodama simuliacija, jei keitėsi atnaujina info.
+            
+        }
 	}
 }
