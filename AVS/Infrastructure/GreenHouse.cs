d906689c906ad9ad75b941f4af34fa5f7d976af5
kruopalt <dogas20@gmail.com>
2017-04-11 21:08:18 +0300

    siek tiek priedu , bet vis dar pradžioje

--- AVS/Infrastructure/GreenHouse.cs
+++ AVS/Infrastructure/GreenHouse.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace AVS
@@ -13,19 +14,30 @@ namespace AVS
         public int shelfAmount { get; private set; }
         public int panelsAmount { get; private set; }
         public int freePanelsAmount { get; private set; }
-        public double freeRation { get; private set; } // panelsAmount/freePanelsAmount
+        public double freeRatio { get; private set; } // panelsAmount/freePanelsAmount
         public GreenHouse(string pavadinimas,int sectCount, int shleftPerSection, int panelsPerShelf)
         {
             this.Pavadinimas = pavadinimas;
+            this.shelfAmount = sectCount*shleftPerSection;
             this.panelsAmount = sectCount * shleftPerSection * panelsPerShelf;
             this.freePanelsAmount = panelsAmount;
-            freeRation = 1;
+            freeRatio = 1;
 
             sections = new Section[sectCount];
             for (int i = 0; i < sectCount; i++)
             {
                 sections[i] = new Section(shleftPerSection, panelsPerShelf);
             }
+        }
+        public void AddPlant(int sectionNo, int shelfNo, int panelNo)
+        {
+            sections[sectionNo].AddPlant(shelfNo,panelNo);
+            freePanelsAmount--;
+
+        }
+        internal static void LoadPlantsFromFile(string plantDataPath)
+        {
+            string[] lines = File.ReadAllLines(plantDataPath);
         }
     }
 }
